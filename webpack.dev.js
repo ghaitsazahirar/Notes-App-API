a1413ac3573b76d498df06b7239b10d357048418
Ghaitsa Zahira Rahma <101691351+ghaitsazahirar@users.noreply.github.com>
2024-04-04 17:21:53 +0700

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,11 @@
-const {merge} = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: 'development'
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    port: 8080,
+  },
 });
